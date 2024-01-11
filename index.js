d36c4e41f1a135b57147c911dadb8bb2f8b60555
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-01-11 15:26:53 -0800

    Practical Math without conditions and functions

--- index.js
+++ index.js
@@ -51,6 +51,10 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 /******************************************************************************************* 
  *                                Part 1: Math Problems
 ********************************************************************************************/
+console.log(`
+    ********************************
+    *****Part 1*********************
+    ********************************`);
 // Check if all numbers are divisible by 5. Cache the result in a variable.
 const isDivBy5 = n1 % 5 == 0 && n2 % 5 == 0 && n3 % 5 == 0 && n4 % 5 == 0;
 console.log(`All are divisible by 5: ${isDivBy5}`); //check answer in console
@@ -77,6 +81,10 @@ console.log(`isValid with allLess25: ${isValid}`); //check answer in console
 /******************************************************************************************* 
  *                                Part 2: Practical Math
 ********************************************************************************************/
+console.log(`
+    ********************************
+    *****Part 2*********************
+    ********************************`);
 /**Input data
 *You are planning a cross - country road trip!
 *The distance of the trip, in total, is 1, 500 miles.
@@ -88,9 +96,70 @@ console.log(`isValid with allLess25: ${isValid}`); //check answer in console
 *The average cost of fuel is $3 per gallon.
  */
 
+//Declare non changeable variables
+const distance = 1500;
+const budget = 175;
+const fuelCost = 3;
+
+//Declare changeable variables
+let pace;
+let time;
+let fuelAmount;
 
 // How many gallons of fuel will you need for the entire trip ?
 //Will your budget be enough to cover the fuel expense ?
 //How long will the trip take, in hours ?
 //Compare the results when traveling at an average of 55, 60, and 75 miles per hour.Which makes the most sense for the trip ?
 //Log the results of your calculations using string concatenation or template literals.
+
+const maxFuelAmount = budget / fuelCost; //estimate max abount of fuel that we could buy
+console.log(`We could afford ${maxFuelAmount} gallons for this trip.`);
+
+//If pace is 55 miles per hour you get 30 miles per gallon
+
+pace = 55;
+fuelAmount = distance / 30;
+time = distance/pace;
+const remainingFuel55 = maxFuelAmount - fuelAmount;
+
+console.log(`
+If pace is 55 miles per hour:
+Trip will take us ${time} hours.
+We need ${fuelAmount} gallons of fuel.
+Will our budget enough?: ${remainingFuel55 >= 0 }`);
+
+// If pace is 60 miles per hour, you get 28 miles per gallon
+
+pace = 60;
+fuelAmount = distance / 28;
+time = distance / pace;
+const remainingFuel60 = maxFuelAmount - fuelAmount;
+
+console.log(`
+If pace is 55 miles per hour:
+Trip will take us ${time} hours.
+We need ${fuelAmount} gallons of fuel.
+Will our budget enough?: ${remainingFuel60 >= 0}`);
+
+// If pace is 75 miles per hour, you get 23 miles per gallon
+
+pace = 75;
+fuelAmount = distance / 23;
+time = distance / pace;
+const remainingFuel75 = maxFuelAmount - fuelAmount;
+
+console.log(`
+If pace is 55 miles per hour:
+Trip will take us ${time} hours.
+We need ${fuelAmount} gallons of fuel.
+Will our budget enough?: ${remainingFuel75 >= 0} `);
+
+const is55PaceBetter = remainingFuel55 > remainingFuel60 && remainingFuel55 > remainingFuel75;
+const is60PaceBetter = remainingFuel60 > remainingFuel55 && remainingFuel60 > remainingFuel75;
+const is75PaceBetter = remainingFuel75 > remainingFuel60 && remainingFuel75 > remainingFuel55;
+
+console.log(`
+Let's choose te best pace:
+Is 55 miles per gallon better pace? - ${is55PaceBetter}
+Is 60 miles per gallon better pace? - ${is60PaceBetter}
+Is 75 miles per gallon better pace? - ${is75PaceBetter}`);
